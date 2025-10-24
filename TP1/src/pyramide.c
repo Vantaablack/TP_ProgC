3e850958d3d68afd80a721a3f5fb7e2ceb7d1a5e
Razak <157386561+Vantaablack@users.noreply.github.com>
2025-10-24 11:27:34 +0200

    pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int n = 5; 
+    int i, j;
+
+    printf("=== Génération d'une pyramide de hauteur %d ===\n\n", n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n - i; j++) {
+            printf("  "); 
+        }
+        for (j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    printf("\nPyramide terminée avec succès !\n");
+
+    return 0;
+}
 
